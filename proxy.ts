c0518e00519549a599c7aaf573e04dd47e065633
Abhishek <abhiarsi2016@gmail.com>
2026-08-04 13:27:16 -0500

    Bypassed auth middleware for crawler files

--- proxy.ts
+++ proxy.ts
@@ -47,6 +47,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|robots\\.txt|sitemap\\.xml|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
